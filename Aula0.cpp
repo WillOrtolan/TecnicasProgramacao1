c73b78ed811a33a7a1aa08318118ae22ca922098
Aluno <ortolan5@hotmail.com>
2025-08-13 19:35:47 -0300

    Primeiro commit

---
+++ Aula0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1 = 0;
+    int numero2 = 0;
+
+    cout << "informe o primeiro numero \n";
+    cin >> numero1;
+    cout << "informe o segundo numero \n";
+    cin >> numero2;
+
+    cout << "os numeros digitados sao: " << numero1 << " e " << numero2;
+    return 0;
+};
